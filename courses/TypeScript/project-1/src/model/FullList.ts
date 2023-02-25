44b2a7e87b3c3e395f1744bd3bde0bac28e9c6ba
MonikaYancheva <monika.milcheva.yancheva@gmail.com>
2023-02-25 19:43:45 +0200

    Project 1

--- courses/TypeScript/project-1/src/model/FullList.ts
+++ courses/TypeScript/project-1/src/model/FullList.ts
@@ -1,7 +1,5 @@
 import ListItem from './ListItem';
 
-const item = new ListItem();
-
 interface List {
   list: ListItem[];
   load(): void;
@@ -12,7 +10,41 @@ interface List {
 }
 
 export default class FullList implements List {
-  static instance: FullList = newFullList();
+  static instance: FullList = new FullList();
 
-  private constructor(private list: ListItem[] = []) {}
+  private constructor(private _list: ListItem[] = []) {}
+
+  get List(): ListItem[] {
+    return this._list;
+  }
+
+  load(): void {
+    const storedList: string | null = localStorage.getItem('myList');
+    if (typeof storedList !== 'string') return;
+
+    const parsedList: { _id: string; _item: string; _checked: boolean }[] = JSON.parse(storedList);
+    parsedList.forEach(itemObj => {
+      const newListItem = new ListItem(itemObj._id, itemObj._item, itemObj._checked);
+      FullList.instance.addItem(newListItem);
+    });
+  }
+
+  save(): void {
+    localStorage.setItem('myList', JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter(item => item.id !== id);
+    this.save();
+  }
 }
