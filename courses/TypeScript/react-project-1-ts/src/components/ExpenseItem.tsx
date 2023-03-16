f2bb9e71c58de401db82151a8d75a3fd52d36869
MonikaYancheva <monika.milcheva.yancheva@gmail.com>
2023-03-17 01:36:57 +0200

    react+ts

---
+++ courses/TypeScript/react-project-1-ts/src/components/ExpenseItem.tsx
@@ -0,0 +1,30 @@
+import './ExpenseItem.css';
+
+interface ExpenseItemProps {
+  date: Date;
+  title: string;
+  amount: number;
+}
+
+const ExpenseItem: React.FC<ExpenseItemProps> = ({ date, title, amount }) => {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
+
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
